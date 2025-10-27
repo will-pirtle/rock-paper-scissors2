93026674cc86d7c37231ca668a6f0163c1d181a6
Will Pirtle <67991566+will-pirtle@users.noreply.github.com>
2025-10-27 09:52:27 -0700

    Refactor to seperate score updating to own function

--- script.js
+++ script.js
@@ -7,15 +7,7 @@ playerChoiceBtns.forEach((button) => {
   button.addEventListener('click', (e) => {
     const playerChoice = e.target.id;
     let roundWinner = playRound(playerChoice, getComputerChoice());
-    if (roundWinner == 'player') {
-      playerScore.textContent = Number(playerScore.textContent) + 1;
-      messageBox.textContent = "You win!";
-    } else if (roundWinner == 'computer') {
-      computerScore.textContent = Number(computerScore.textContent) + 1;
-      messageBox.textContent = "You lose...";
-    } else if (roundWinner == 'tie') {
-      messageBox.textContent = "Draw. Try again.";
-    }
+    updateScoreBoard(roundWinner);
 
     // check for game win
     if (playerScore.textContent == 5) {
@@ -62,43 +54,15 @@ function playRound(playerChoice, computerChoice) {
   }
 }
 
-// Play whole game
-function playGame() {
-  let playerScore = 0;
-  let computerScore = 0;
-  let roundsRemaining = 5;
-
-  while (roundsRemaining > 0) {
-    // Get new player choice and computer choice
-    const playerSelection = getPlayerChoice();
-    const computerSelection = getComputerChoice();
-
-    // Play a round and store result in 'roundWinner'
-    let roundWinner = playRound(playerSelection, computerSelection);
-
-    if (roundWinner == 'player') {
-      playerScore++;
-      console.log('You won!');
-      console.log(`Player score: ${playerScore}`);
-      console.log(`Computer score: ${computerScore}`);
-      roundsRemaining--;             
-    } else if (roundWinner == 'computer') {
-      computerScore++;
-      console.log('You lost.');
-      console.log(`Player score: ${playerScore}`);
-      console.log(`Computer score: ${computerScore}`);
-      roundsRemaining--;
-    } else {
-      console.log('You tied. Replay round.')
-    }
+// Update score
+function updateScoreBoard(winner) {
+  if (winner == 'player') {
+    playerScore.textContent = Number(playerScore.textContent) + 1;
+    messageBox.textContent = "You win!";
+  } else if (winner == 'computer') {
+    computerScore.textContent = Number(computerScore.textContent) + 1;
+    messageBox.textContent = "You lose...";
+  } else if (winner == 'tie') {
+    messageBox.textContent = "Draw. Try again.";
   }
-
-  // End the game
-  console.log('GAME OVER!');
-  if (playerScore > computerScore) {
-    console.log('You won! Congrats!');
-  } else {
-    console.log('You lost. Better luck next time.')
-  }  
 }
-
